abaa1a1f89983ead9267e6216a2024ba6f55e186
Harold <haroldcj95@gmail.com>
2019-02-12 01:09:56 -0400

    added tests for the words FizzBuzz

--- test/fizzbuzz_test.js
+++ test/fizzbuzz_test.js
@@ -5,4 +5,10 @@ describe('Fizzbuzz Function Tests', function () {
     it('Should return FizzBuzz when the input is "0" ', function () {
         assert.equal(fizzBuzzFunctions.fizzbuzz(0), "FizzBuzz");
     });
+    it('Should return Fizz when the input is "3" ', function () {
+        assert.equal(fizzBuzzFunctions.fizzbuzz(3), "Fizz");
+    });
+    it('Should return Buzz when the input is "5" ', function () {
+        assert.equal(fizzBuzzFunctions.fizzbuzz(5), "Buzz");
+    });
 });
