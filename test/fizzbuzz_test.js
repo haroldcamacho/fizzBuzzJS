6b557e51937995de17c89893382a3b5106e81933
Harold <haroldcj95@gmail.com>
2019-02-12 01:11:19 -0400

    added tests for other values

--- test/fizzbuzz_test.js
+++ test/fizzbuzz_test.js
@@ -11,4 +11,16 @@ describe('Fizzbuzz Function Tests', function () {
     it('Should return Buzz when the input is "5" ', function () {
         assert.equal(fizzBuzzFunctions.fizzbuzz(5), "Buzz");
     });
+    it('Should return FizzBuzz when the input is " 15" ', function () {
+        assert.equal(fizzBuzzFunctions.fizzbuzz(15), "FizzBuzz");
+    });
+    it('Should return "1" when the input is " 1" ', function () {
+        assert.equal(fizzBuzzFunctions.fizzbuzz(1), 1);
+    });
+    it('Should return "2" when the input is " 2" ', function () {
+        assert.equal(fizzBuzzFunctions.fizzbuzz(2), 2);
+    });
+    it('Should return the number when the input is not a multiple of 5 or 3 like "17" ', function () {
+        assert.equal(fizzBuzzFunctions.fizzbuzz(17), 17);
+    });
 });
