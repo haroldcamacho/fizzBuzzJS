e97a4f456864826af0dfa318b3a4839827a6a9c8
Harold <haroldcj95@gmail.com>
2019-02-12 01:08:25 -0400

    added first test

--- test/fizzbuzz_test.js
+++ test/fizzbuzz_test.js
@@ -2,4 +2,7 @@ var assert = require('assert');
 var fizzBuzzFunctions = require("../fizzbuzz.js")
 
 describe('Fizzbuzz Function Tests', function () {
+    it('Should return FizzBuzz when the input is "0" ', function () {
+        assert.equal(fizzBuzzFunctions.fizzbuzz(0), "FizzBuzz");
+    });
 });
