50a5e01cab152c367dd79b9d59a8fd684b394258
Harold <haroldcj95@gmail.com>
2019-02-12 01:16:19 -0400

    added tests for the generated sequences

--- test/fizzbuzz_test.js
+++ test/fizzbuzz_test.js
@@ -47,5 +47,32 @@ describe('Fizzbuzz Function Tests', function () {
             assert.equal(resultSequence[0], "FizzBuzz");
             assert.equal(resultSequence[1], 1);
         });
+        it('Should return a Sequence of size 3, when the input is 3" ', function () {
+            let resultSequence=fizzBuzzFunctions.generateFizzbuzzSequence(3)
+            assert.equal(resultSequence.length, 3);
+        });
+        it('Should contain the word FizzBuzz and the values "1" and "2" , when the input is "3" in the positions "0", "1" and "2" respectively" ', function () {
+            let resultSequence=fizzBuzzFunctions.generateFizzbuzzSequence(3)
+            assert.equal(resultSequence[0], "FizzBuzz");
+            assert.equal(resultSequence[1], 1);
+            assert.equal(resultSequence[2], 2);
+        });
+        it('Should return a Sequence of size 4, when the input is 4" ', function () {
+            let resultSequence=fizzBuzzFunctions.generateFizzbuzzSequence(4)
+            assert.equal(resultSequence.length, 4);
+        });
+        it('Should contain the word FizzBuzz, the values "1",  "2" and the word "Fizz" , when the input is "4" in the positions "0", "1", "2" and "3" respectively" ', function () {
+            let resultSequence=fizzBuzzFunctions.generateFizzbuzzSequence(4)
+            assert.equal(resultSequence[0], "FizzBuzz");
+            assert.equal(resultSequence[1], 1);
+            assert.equal(resultSequence[2], 2);
+            assert.equal(resultSequence[3], "Fizz");
+        });
+        it('Should contain the words FizzBuzz, "Fizz" and "Buzz" , when the input is "6" in the positions "0", "3" and "5" respectively" ', function () {
+            let resultSequence=fizzBuzzFunctions.generateFizzbuzzSequence(6)
+            assert.equal(resultSequence[0], "FizzBuzz");
+            assert.equal(resultSequence[3], "Fizz");
+            assert.equal(resultSequence[5], "Buzz");
+        });
     });
 });
